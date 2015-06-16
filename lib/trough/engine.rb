2b7b5fa1d5fea32101609330240341882c19612c
Greg Woodcock <greg@yoomee.com>
2015-06-16 15:20:46 +0100

    Extend the pig field types with a document type

--- lib/trough/engine.rb
+++ lib/trough/engine.rb
@@ -1,9 +1,25 @@
 module Trough
   class Engine < ::Rails::Engine
     isolate_namespace Trough
-    
+
+    config.after_initialize do
+      if defined? ::Pig
+        ::Pig::ContentAttribute.class_eval do
+          class << self
+            prepend ::Trough::ExtraPigFieldTypes
+          end
+        end
+      end
+    end
+
     rake_tasks do
       Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
+    end
+  end
+
+  module ExtraPigFieldTypes
+    def field_types
+      super.merge(document: "Document")
     end
   end
 end
