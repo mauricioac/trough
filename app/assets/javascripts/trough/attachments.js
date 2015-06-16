044825a9d801c9b27a70384f4ffc57448133b01a
Greg Woodcock <greg@yoomee.com>
2015-06-16 11:57:42 +0100

    Slide down attributes once file is chosen

--- app/assets/javascripts/trough/attachments.js
+++ app/assets/javascripts/trough/attachments.js
@@ -1,14 +1,15 @@
-var attachments = (function () {   
+var attachments = (function () {
 
   $(document).on("upload:start", "form#new_document", function(event) {
     var $progress = $('.progress', event.currentTarget);
     var $progressBar = $('.progress-bar', $progress);
-    $progressBar.css("width", '0%');
+    $("#document_description_input").slideDown();
+    $progressBar.css("width", '10%');
   });
 
   $(document).on("upload:progress", "form#new_document", function(event) {
     var detail = event.originalEvent.detail;
-    var percentage = Math.round( (detail.loaded / detail.total) * 100); 	
+    var percentage = Math.round( (detail.loaded / detail.total) * 100);
     $('.progress-bar', event.currentTarget).css('width', percentage+'%');
   });
 
@@ -16,13 +17,13 @@ var attachments = (function () {
     $form = $(event.currentTarget);
     $('#document_submit_action', $form).removeAttr('disabled');
   });
-  
+
   // Rails_ujs fires this event and submits the form normally if a remote form has a file chosen.
   // This is because by default rails cant upload files via ajax, in our case the file is uploaded directly to S3
   // and so this is'nt an issue.
   $(document).on('ajax:aborted:file', 'form', function(){
    var form = $(this);
-   
+
    // Manually trigger the rails ajax form submit
    $.rails.handleRemote(form);
    return false;
