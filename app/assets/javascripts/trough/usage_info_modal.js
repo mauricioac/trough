3e41ebc8d88a9842655e5dcdc97d34c377f76b36
Greg Woodcock <greg@yoomee.com>
2015-06-29 16:59:54 +0100

    add js for info modal

---
+++ app/assets/javascripts/trough/usage_info_modal.js
@@ -0,0 +1,35 @@
+var DocumentInfoModal = (function () {
+  "use strict";
+
+  $(document).ready(function(){
+    $('[data-document-info-modal]').on('click', function (event) {
+      event.preventDefault();
+      var openingLink = $(event.target);
+      $.get('/documents/' + openingLink.parent().data('document-info-modal') + '/info', function(data){
+        var compiled = _.template("\
+          <h3>Description</h3>\
+          <div class='document-description'> <%= data.description %> </div>\
+          <table id='usage_link_list' class='cms-table'>\
+          <thead>\
+            <tr>\
+              <th>Pages</th>\
+              <th>Active</th>\
+              <th>Download count</th>\
+            </tr>\
+          </thead>\
+          <% _.each(data.document_usages, function(usage) {  %>\
+            <tr>\
+            <td><%= usage.content_package.name %></td>\
+            <td><%= usage.active %></td>\
+            <td><%= usage.download_count %></td>\
+            </tr>\
+          <% }); %>\
+          </table>\
+          <a class='btn btn-primary pull-left' href=\"/documents/<%= data.slug %>\" >Download</a>\
+        ", {variable: 'data'});
+        $('#usageLinks .modal-body').html(compiled(data));
+      });
+      $('#usageLinks').modal("show");
+    });
+  });
+})();
