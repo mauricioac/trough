9bbed01235d973874735b6ff42a5c8dc8a29dab2
Jon Kyte <jon@yoomee.com>
2015-07-21 15:01:16 +0100

    Style document upload modals / index

--- app/assets/javascripts/trough/usage_info_modal.js
+++ app/assets/javascripts/trough/usage_info_modal.js
@@ -13,26 +13,26 @@ var DocumentInfoModal = (function () {
     $.get('/documents/' + slug + '/info', function(data) {
       var compiled = _.template(
         "<div class='row'>" +
-          "<div class='col-md-5 col-md-offset-1'>" +
+          "<div class='col-md-6'>" +
             "<label class='col-primary'>Filename</label>" +
-            "<p><%= data.file_filename %></p>" +
+            "<p><i><%= data.file_filename %></i></p>" +
           "</div>" +
-          "<div class='col-md-5'>" +
+          "<div class='col-md-6'>" +
             "<label class='col-primary'>Filesize</label>" +
-            "<p><%= data.file_size %></p>" +
+            "<p><i><%= data.file_size %></i></p>" +
           "</div>" +
         "</div>" +
         "<div class='row'>" +
-          "<div class='col-md-5 col-md-offset-1'>" +
+          "<div class='col-md-6'>" +
             "<label class='col-primary'>Author</label>" +
-            "<p><%= data.uploader %></p>" +
+            "<p><i><%= data.uploader %></i></p>" +
           "</div>" +
-          "<div class='col-md-5'>" +
+          "<div class='col-md-6'>" +
             "<label class='col-primary'>Uploaded</label>" +
-            "<p><%= data.uploaded_on %></p>" +
+            "<p><i><%= data.uploaded_on %></i></p>" +
           "</div>" +
         "</div>" +
-        "<label class='control-label'>description</label>" +
+        "<label class='col-primary'>description</label>" +
         "<div class='document-description'> <%= data.description %> </div>" +
         "<table id='usage_link_list' class='cms-table'>" +
         "<thead>" +
@@ -50,7 +50,9 @@ var DocumentInfoModal = (function () {
           "</tr>" +
         "<% }); %>" +
         "</table>" +
-        "<a class='btn btn-primary pull-left' href=\"/documents/<%= data.slug %>\" >Download</a>",
+        "<a class='btn btn-primary pull-right' href=\"/documents/<%= data.slug %>\" ><i class='fa fa-download'></i> Download</a>" +
+        "<a class='btn btn-error pull-right' href=''><i class='fa fa-trash-o'></i> Delete</a>" +
+        "<div class='clearfix'></div>",
         {variable: 'data'});
       $('#usageLinks .modal-body').html(compiled(data));
     });
@@ -63,3 +65,6 @@ var DocumentInfoModal = (function () {
   };
 
 })();
+
+// %td.documents-delete-column
+//   =link_to('Delete', document_path(document), method: :delete, remote: true, class: 'fa fa-trash-o document-delete', data: {confirm: 'Are you sure? This cannot be undone'} )
