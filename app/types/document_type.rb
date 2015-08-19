e672513afaabe8401798cfbfda976738a0d5ad00
Nick Bolt <nick@yoomee.com>
2015-08-19 13:55:47 +0100

    add a method to quickly access the document file

--- app/types/document_type.rb
+++ app/types/document_type.rb
@@ -1,4 +1,13 @@
 module Pig
   class DocumentType < Type
+    def decorate(content_package)
+      this = self
+      super(content_package)
+      content_package.define_singleton_method("#{@slug}_file") do
+        id = this.content_value(self)
+        return false unless Trough::Document.exists?(id)
+        Trough::Document.find(id)
+      end
+    end
   end
 end
