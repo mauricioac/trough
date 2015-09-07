c27073d99ec5604d531e056a39914d9060f41141
Nick Bolt <nick@yoomee.com>
2015-09-07 13:25:01 +0100

    better permissions

--- app/models/trough/ability.rb
+++ app/models/trough/ability.rb
@@ -8,8 +8,13 @@ module Trough
 
       return unless user
 
-      if user.role_is?(:developer) || user.role_is?(:admin)
-        can :manage, :all
+      if user.role.in?(%w( developer admin editor author ))
+        can :manage, Trough::Document
+        can :manage, Trough::DocumentUsage
+      end
+
+      if user.role.in?(%w( editor author ))
+        cannot :destroy, Trough::Document
       end
     end
   end
