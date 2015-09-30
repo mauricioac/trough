b8f25a6b479caa9856fe8dedcee86c0162c5ff1d
Greg Woodcock <greg@yoomee.com>
2015-09-30 13:58:36 +0100

    Allow clicking a table header to sort by that column

--- app/controllers/trough/documents_controller.rb
+++ app/controllers/trough/documents_controller.rb
@@ -6,8 +6,11 @@ module Trough
 
     before_action :prepare_new_document, only: [:index, :search]
 
+    helper_method :sort_column, :sort_direction
+
     def index
-      @documents = Document.include_meta.all.order(:slug)
+      docs = Document.include_meta.all
+      @documents = docs.order("LOWER(NULLIF(#{sort_column}, '')) #{sort_direction}")
     end
 
     def search
@@ -78,12 +81,20 @@ module Trough
 
     private
 
-      def document_params
-        params.require(:document).permit(:file, :slug, :description)
-      end
+    def document_params
+      params.require(:document).permit(:file, :slug, :description)
+    end
 
-      def prepare_new_document
-        @document = Document.new
-      end
+    def prepare_new_document
+      @document = Document.new
+    end
+
+    def sort_column
+      Document.column_names.include?(params[:sort]) ? params[:sort] : 'slug'
+    end
+
+    def sort_direction
+      %w(asc desc).include?(params[:direction]) ? params[:direction] : 'asc'
+    end
   end
 end
